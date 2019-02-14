8074fa186a6370e972d607997c19e7bb12e9bc3e
FizzyMo <carisa1000@yahoo.com>
2019-02-14 02:05:45 -0700

    Basic

---
+++ javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    var 1charactCount = true;
+    var countDefending = 0;
+    var obiCount = 0;
+    var lukeCount = 0;
+    var sidCOunt = 0;
+    var darmaulCount = 0;
+
+    $('.attBtn').off('click');
+
+    $('1charact').on('click', function() {
+
+        if(1charactCount ===true)
+
+        {
+            $(this).addClass('main');
+            obiCount++;
+
+            1charactCount = false;
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
